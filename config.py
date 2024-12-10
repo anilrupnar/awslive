0f64ca4f19c78f9e983ed61a5ffa24ccab02d6cd
anil-rupnar <rupnaraniloffice@gmail.com>
2024-12-10 07:02:09 +0530

    first

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.czk0casgmm6r.ap-south-1.rds.amazonaws.com"
+customuser = "admindemo"
+custompass = "admindemo8530"
+customdb = "employee"
+custombucket = "addemployeelive"
+customregion = "ap-south-1"
